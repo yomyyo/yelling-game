3fbba94a5b69db9efc151a4a236b00213a803bc9
Hunter Wiegand <hunter_wiegand@yahoo.com>
2019-04-12 13:32:56 -0700

    Added base html

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # yelling-game
+
+npm init -y
+npm install dotenv
+npm install express
+npm install express-handlebars
+npm install mysql2
+npm install sequelize
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,11 @@ module.exports = function(app) {
     });
   });
 
+  //Load create game page
+  app.get("/create-game", function(req, res) {
+    res.render("create-game");
+  });
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
