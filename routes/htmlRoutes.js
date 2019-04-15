0babf3f98dd4f4ab473fa7ca7cfc88842cfa5a63
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2019-04-15 11:59:29 -0700

    sequelize working better

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,22 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-      res.render("index", {
-      });
+  app.get("/", function (req, res) {
+    db.Player.findAll({}).then((res)=>{
+      console.log(res);
+    });
+    res.render("index", {
+    });
   });
 
   //Load create game page
-  app.get("/create-game", function(req, res) {
+  app.get("/create-game", function (req, res) {
     res.render("create-game");
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
