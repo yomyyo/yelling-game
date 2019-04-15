e4226052b4b65d9accaa4a16efacf6f7a66a58a8
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2019-04-15 11:47:38 -0700

    html routes working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,26 +3,13 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
       });
-    });
   });
 
   //Load create game page
   app.get("/create-game", function(req, res) {
     res.render("create-game");
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
   });
 
   // Render 404 page for any unmatched routes
