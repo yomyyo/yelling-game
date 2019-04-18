ed7461a3eaa4745af9f868037d04dc5e0cd1719f
Sammy Tang <samtang226@gmail.com>
2019-04-18 13:18:25 -0700

    added server integration

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,22 +3,24 @@ var db = require("../models");
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
-    res.render('index');
-    // db.Player.findAll({}).then((data)=>{
-    //   // console.log(res);
-    //   res.render("index", {
-    // });
-    // });
-    
+    //   res.render('index');
+    db.Player.findAll({
+      order: [
+        ['wins', 'DESC'],
+        ["losses", "ASC"]
+      ]
+    }).then((data) => {
+      // console.log(res);
+      res.render("index", { player: data });
+    });
   });
-
   //Load create game page
   app.get("/create-game", function (req, res) {
     res.render("create-game");
   });
 
   //Load BLOBTEST page
-  app.get("/test", function(req, res) {
+  app.get("/test", function (req, res) {
     res.render("TEST");
   })
 
@@ -28,8 +30,8 @@ module.exports = function (app) {
   })
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
